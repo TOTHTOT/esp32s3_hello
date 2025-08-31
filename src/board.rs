58347c6e402d30d2de50969440d3b496233878d9
TOTHTOT <mczyfs@gmail.com>
2025-08-31 14:21:25 +0800

    添加功能 1. 使用 esp32_nimble 作为ble库, 还原对wifi的更改.

--- src/board.rs
+++ src/board.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, Result};
 use embedded_svc::wifi::Configuration;
+use esp32_nimble::{uuid128, BLEAdvertisedDevice, BLEDevice, BLEScan};
 use esp_idf_svc::hal::prelude::Peripherals;
+use esp_idf_svc::hal::task::block_on;
 use esp_idf_svc::{
-    bt::*,
     eventloop::EspSystemEventLoop,
     nvs::EspNvsPartition,
     wifi::{AuthMethod, ClientConfiguration, EspWifi},
@@ -15,8 +16,7 @@ const WIFI_SSID: &str = "esp32_2.4G";
 const WIFI_PASSWD: &str = "12345678..";
 pub struct BspEsp32S3CoreBoard<'d> {
     pub ws2812: Ws2812Esp32Rmt<'d>,
-    pub wifi: Option<EspWifi<'d>>,
-    pub ble: Option<BtDriver<'d, Ble>>,
+    pub wifi: EspWifi<'d>,
     wifi_ssid: String,
     wifi_password: String,
 }
@@ -26,21 +26,15 @@ impl<'d> BspEsp32S3CoreBoard<'d> {
     pub fn new(peripherals: Peripherals) -> Result<Self> {
         let sysloop = EspSystemEventLoop::take()?;
         let nvs = EspNvsPartition::<NvsDefault>::take()?;
-        let mut wifi = None;
-        let mut ble = None;
 
-        // 目前所知, wifi和ble可能不能同时使用因为 modem 不支持 copy 和 Arc 特性, 只能被一个设备持有
-        if false {
-            wifi = Some(EspWifi::new(peripherals.modem, sysloop, Some(nvs.clone()))?);
-        } else {
-            ble = Some(BtDriver::<Ble>::new(peripherals.modem, Some(nvs.clone()))?);
-        }
+        let wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs.clone()))?;
+        Self::ble_scan(10000)?;
+        log::info!("start init ws2812");
         let ws2812 = Ws2812Esp32Rmt::new(peripherals.rmt.channel0, peripherals.pins.gpio48)
             .map_err(|e| anyhow!("Ws2812Esp32Rmt error: {:?}", e))?;
         Ok(Self {
             ws2812,
             wifi,
-            ble,
             wifi_ssid: WIFI_SSID.to_string(),
             wifi_password: WIFI_PASSWD.to_string(),
         })
@@ -51,14 +45,13 @@ impl<'d> BspEsp32S3CoreBoard<'d> {
         wifi_ssid: String,
         wifi_passwd: String,
     ) -> Result<(), anyhow::Error> {
-        let wifi = self.wifi.as_mut().ok_or(anyhow!("wifi unavailable"))?;
-        if wifi.is_connected()? {
+        if self.wifi.is_connected()? {
             log::info!("wifi is connected, now disconnecting");
-            wifi.disconnect()?;
+            self.wifi.disconnect()?;
         }
 
         log::info!("wifi start");
-        wifi.start()?;
+        self.wifi.start()?;
         // 构造wifi名字和密码
         let mut ssid = heapless::String::<32>::new();
         let _ = ssid.push_str(wifi_ssid.as_str());
@@ -66,15 +59,40 @@ impl<'d> BspEsp32S3CoreBoard<'d> {
         let _ = password.push_str(wifi_passwd.as_str());
         self.wifi_password = wifi_passwd;
         self.wifi_ssid = wifi_ssid;
-        wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-            ssid,
-            password,
-            auth_method: AuthMethod::WPA2Personal,
-            ..Default::default()
-        }))?;
+        self.wifi
+            .set_configuration(&Configuration::Client(ClientConfiguration {
+                ssid,
+                password,
+                auth_method: AuthMethod::WPA2Personal,
+                ..Default::default()
+            }))?;
         log::info!("wifi start connect");
-        wifi.connect()?;
+        self.wifi.connect()?;
         Ok(())
+    }
+    /// 扫描附近蓝牙, 并返回扫描结果类型: BLEAdvertisedDevice
+    pub fn ble_scan(scan_time: i32) -> Result<Vec<BLEAdvertisedDevice>, anyhow::Error> {
+        let ble = BLEDevice::take();
+        let mut ble_scan = BLEScan::new();
+        let mut devices = Vec::new();
+        log::info!("BleScanning...");
+        block_on(async {
+            ble_scan
+                .active_scan(true)
+                .interval(1000)
+                .window(99)
+                .start(ble, scan_time, |ble_device, data| {
+                    devices.push(ble_device.clone());
+                    None::<BLEAdvertisedDevice>
+                })
+                .await
+                .unwrap();
+        });
+        log::info!("Ble Scan end");
+        for device in &devices {
+            log::info!("device: {:?}", device);
+        }
+        Ok(devices)
     }
 
     pub fn wifi_ssid(&self) -> &str {
